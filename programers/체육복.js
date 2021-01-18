de6f3db2034a144b4d1ac66d1335a7d9e165155d
Sunjae95 <tjswo9502@gmail.com>
2021-01-19 02:03:47 +0900

    end

---
+++ programers/체육복.js
@@ -0,0 +1,25 @@
+function solution(n, lost, reserve) {
+    const arr = {};
+    let answer = 0;
+    for(let i = 1; i <= n; i++){
+        arr[i] = 1;
+    }
+    lost.forEach(number => arr[number] -= 1);
+    reserve.forEach(number => arr[number] += 1);
+
+    for(let i = 1; i <= n; i++){
+        if(arr[i] === 2 && arr[i-1] === 0){
+                arr[i-1]++;
+                arr[i]--;
+        } else if(arr[i] === 2 && arr[i+1] === 0){
+                arr[i+1]++;
+                arr[i]--;
+        }
+    }
+    for(let key in arr){
+        if(arr[key] > 0){
+            answer++;
+        }
+    }
+    return answer;
+}
