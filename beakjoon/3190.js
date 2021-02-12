a9ee76adceb26d438bdc1f213503e5fb2aff2fb5
Sunjae95 <tjswo9502@gmail.com>
2021-02-12 18:27:06 +0900

    3190 baekjoon

--- beakjoon/3190.js
+++ beakjoon/3190.js
@@ -3,70 +3,65 @@ const fs = require("fs");
 let input = fs.readFileSync('input').toString().split('\r\n');
 // 백준입력
 // let input = fs.readFileSync('/dev/stdin').toString().split('\n');
-//게임판 만들기 벽을 만들기 위해 +2
-const xy = +input[0]+2;
+
+//게임판 만들기 인덱스 그대로 받아오기 위해 +1
+const xy = +input[0]+1;
 let board = Array(xy).fill(null).map(() => Array(xy).fill(false));
+
 //사과 개수 만큼 게임판에 배치
 const totalapple = +input[1];
 for(let i=0; i<totalapple; i++){
     const [ row, col ] = input[2+i].split(" ");
-    board[row][col] = true;
+    board[row][col] = 'A';
 }
 
-
 class Snake{
-    constructor(row, col, drow, dcol){
+    constructor(row, col){
         this.row = row;
         this.col = col;
-        this.drow = drow;
-        this.dcol = dcol;
     }
 }
 
 //방향 큐 설정
 class dict {    
-    constructor(x, c){
-        this.x = x;
-        this.c = c;
+    constructor(X, C){
+        this.X = X;
+        this.C = C;
     }
 }
-let dictQ = [];
-
-for(let i=totalapple+3; i<input.length; i++){
-    const [ x, c ] = input[i].split(" ");
-    dictQ.push(new dict(+x,c));
+let dicQueue = [];
+for(let i=totalapple+3; i<input.length; i++){       //방향 큐 저장
+    const [ X, C ] = input[i].split(" ");
+    dicQueue.push(new dict(+X,C));
 }
-
-let row=1, col=1, drow=0, dcol=1;
+//초기 위치, 방향, 시간 
+let row=1, col=1, drow=0, dcol=1, time = 0;
 let q = [];
-let time = 0;
-q.push(new Snake(row, col, drow, dcol));
-
+q.push(new Snake(row, col));
 
 while(true){
     //시간 증가
     time++;
-    //이동
-    board[row][col] = 0;
+    //이동 
+    board[row][col] = true;
     row+=drow;
     col+=dcol;
-    q.push(new Snake(row, col, drow, dcol));
-    
-    
-    //이동 후 끝나는 조건 설정 
-    if( row < 1 || row >= xy || col < 1 || col >= xy || board[row][col] === 0 ) {   //벽, 자기 자신에 부딪힐 때
+    //이동 전 끝나는 조건 설정 
+    if( row < 1 || row >= xy || col < 1 || col >= xy || board[row][col] === true ) {   //벽, 자기 자신에 부딪힐 때
         break;
+    } else {
+        q.push(new Snake(row, col)); 
     }
     
-    if( board[row][col] === false ){     //사과가 없으면 꼬리칸 이동
+    if( board[row][col] !== 'A' ){     //사과가 없으면 꼬리칸 이동
         const tail =q.shift();
         board[tail.row][tail.col] = false;
     } 
 
     //방향 바꾸기
-    if(dictQ[0] !== undefined){
-        if( dictQ[0].x === time ){
-            if( dictQ[0].c === 'L'){   //왼쪽회전
+    if(dicQueue[0] !== undefined){
+        if( dicQueue[0].X === time ){
+            if( dicQueue[0].C === 'L'){   //왼쪽회전
                 if( drow === 0 && dcol === 1){  //오른쪽 -> 위쪽
                     drow = -1;
                     dcol = 0;
@@ -80,7 +75,7 @@ while(true){
                     drow = 0;
                     dcol = 1;
                 }
-            }else if (dictQ[0].c === 'D'){     //오른쪽
+            }else if (dicQueue[0].C === 'D'){     //오른쪽
                 if( drow === 0 && dcol === 1){  //오른쪽 -> 아래쪽
                     drow = 1;
                     dcol = 0;
@@ -95,9 +90,9 @@ while(true){
                     dcol = 1;
                 }
             }
-            dictQ.shift();
+            dicQueue.shift();
         }
     } 
-    
 }
 console.log(time);
+
